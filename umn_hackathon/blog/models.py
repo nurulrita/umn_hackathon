a5e1a861ff6989edbdb25dce695312d2f7e8ca8c
nuthanu <nuthanuu@gmail.com>
2016-11-19 09:59:51 +0700

    first commit

---
+++ umn_hackathon/blog/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.utils import timezone
+
+from umn_hackathon.users.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+    image = models.ImageField()
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+def approved_comments(self):
+    return self.comments.filter(approved_comment=True)
